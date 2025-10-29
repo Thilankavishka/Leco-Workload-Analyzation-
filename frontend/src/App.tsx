5ae8a4bcf7bacfd62fd784ea55cc882b8f52f2f3
Thilankavishka <thilankavishka25@gmail.com>
2025-10-30 00:21:44 +0530

    frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home-page";
+import AnalysisPage from "./pages/analysis-page";
+import BlockDetailsPage from "./pages/block-details-page";
+import ComparisonPage from "./pages/comparison-page";
+import EmployeeAnalysisPage from "./pages/employee-analysis-page";
+import TaskDetailsPage from "./pages/task-details-page";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/analysis" element={<AnalysisPage />} />
+        <Route path="/block/:blockId" element={<BlockDetailsPage />} />
+        <Route path="/comparison" element={<ComparisonPage />} />
+        <Route path="/block/:blockId/employee/:employeeId" element={<EmployeeAnalysisPage />} />
+        <Route path="/block/:blockId/task/:taskId" element={<TaskDetailsPage />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
