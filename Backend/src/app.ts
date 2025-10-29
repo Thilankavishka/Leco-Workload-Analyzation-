f812747086719103d0ca40485ae176ceb8907124
Thilankavishka <thilankavishka25@gmail.com>
2025-10-29 20:05:37 +0530

    Add nodemon and additional dependencies to package-lock.json; update app.ts to include employee route

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import logsRoute from './routes/logsRoute';
 import performanceRoute from './routes/performanceRoute';
+import employeeroute from './routes/employees';
 
 dotenv.config();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 
 app.use('/logs', logsRoute);
 app.use('/performance', performanceRoute);
+app.use('/employees', employeeroute);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
