719f388a17191f760353964cd142eb924442f323
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-11-03 22:31:18 +0530

    chore: update main-route.ts to include performance history and task assignments routers

--- backend/src/routes/main-route.ts
+++ backend/src/routes/main-route.ts
@@ -1,18 +1,22 @@
 /**
  * main-route.ts
  *
- * @update 11/03/2025
+ * @updated 11/03/2025
  */
 
 import { Router } from "express";
 import blockRouter from "./block-route";
 import employeeRouter from "./employee-route";
 import taskRouter from "./task-route";
+import performanceHistoryRouter from "./performance-history-routes";
+import taskAssignmentRouter from "./task-assignment-routes";
 
 const router = Router();
 
 router.use("/blocks", blockRouter);
 router.use("/employees", employeeRouter);
 router.use("/tasks", taskRouter);
+router.use("/performance-history", performanceHistoryRouter);
+router.use("/task-assignments", taskAssignmentRouter);
 
 export default router;
