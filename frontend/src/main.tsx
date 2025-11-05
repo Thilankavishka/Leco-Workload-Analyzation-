0d7fd704394cfe85bac229323eea880f1fc3464e
teshankalhara <139575652+teshankalhara@users.noreply.github.com>
2025-11-05 14:13:28 +0530

    feat: wrap App component with BlockProvider for block context management

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import { BlockProvider } from './contexts/block-context.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <BlockProvider>
+        <App />
+      </BlockProvider>
     </BrowserRouter>
   </StrictMode>,
 )
